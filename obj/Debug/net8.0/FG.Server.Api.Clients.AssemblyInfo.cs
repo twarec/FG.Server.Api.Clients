c5b185f2f5722294a382f46deb00a7a8db763a7d
IkinaiT <ikinai@yandex.ru>
2024-10-27 13:31:20 +0300

    upd

--- obj/Debug/net8.0/FG.Server.Api.Clients.AssemblyInfo.cs
+++ obj/Debug/net8.0/FG.Server.Api.Clients.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FG.Server.Api.Clients")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b107fff5ed63936652807f18b1f62b8ae5aa4b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e1c17ff7a1dc8d8d3223fac2a3f7222b2c6ec82")]
 [assembly: System.Reflection.AssemblyProductAttribute("FG.Server.Api.Clients")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FG.Server.Api.Clients")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
