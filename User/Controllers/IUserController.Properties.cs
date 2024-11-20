6f734db7d33ccb38903f30272ba143bdd88f039e
wenawo153 <wenawo153@gmail.com>
2024-11-20 18:02:28 +0300

    w

---
+++ User/Controllers/IUserController.Properties.cs
@@ -0,0 +1,53 @@
+﻿using FG.Server.Api.Clients.User.Models.Request.User;
+using FG.Server.Api.Clients.User.Models.Responce.User;
+using Refit;
+
+namespace FG.Server.Api.Clients.User.Controllers
+{
+	public partial interface IUserController
+	{
+		[Get("/User/Properties")]
+		public Task<UserPropertyResponce> GetProperties(
+		[Body] UserPropertyOptions options);
+
+		[Get("/User/Properties/{userId}")]
+		public Task<UserPropertyResponce> GetPropeties(
+			Guid userId,
+			[Body] UserPropertyOptions options);
+
+		[Get("/User/Properties/Stacks")]
+		public Task<UserStackPropertyResponce> GetAllUserStacksProperties();
+
+		[Get("/User/Properties/Stacks{userId}")]
+		public Task<UserStackPropertyResponce> GetAllUserStacksProperties(
+			Guid userId);
+
+		[Post("/User/Properties")]
+		public Task<UserStackPropertyResponce> SetProperties(
+			[Body] SetUserPropertyOptions options);
+
+		[Get("/User/Properties/Stacks/Names")]
+		public Task<List<string>> GetUserNamesStacks();
+
+		[Get("/User/Properties/Stacks/Names/{userId}")]
+		public Task<List<string>> GetUserNamesStacks(
+			Guid userId);
+
+		[Get("/User/Properties/Stacks/Stack")]
+		public Task<UserStackPropertyResponce> GetUserStackProperties(
+			[Query] string stackName);
+
+		[Get("/User/Properties/Stacks/Stack/{userId}")]
+		public Task<UserStackPropertyResponce> GetUserStackProperties(
+			Guid userId,
+			[Query] string stackName);
+
+		[Delete("/User/Properties")]
+		public Task<UserStackPropertyResponce> RemoveProperties(
+			[Body] UserPropertyOptions options);
+
+		[Delete("/User/Properties/Stack")]
+		public Task RemoveStackProperties(
+			[Query] string stackName);
+	}
+}
--- User/Controllers/IUserController.cs
+++ User/Controllers/IUserController.cs
@@ -6,7 +6,7 @@ using System.Diagnostics.Contracts;
 
 namespace FG.Server.Api.Clients.User.Controllers;
 
-public interface IUserController
+public partial interface IUserController
 {
     [Get("/User")]
     public Task<UserResponce> GetUser(
@@ -36,11 +36,6 @@ public interface IUserController
     public Task<UserResponce> EditUser(
         [Body] EditUserOptions options);
 
-
-    [Get("/User/Connect/AuthMethod/Email")]
-    public Task<AddEmailAuthorizationResponce> AddEmail(
-        [Body] AuthorizationEmailOptions options);
-
     [Put("/User/Password")]
     public Task EditPassword(
         [Body] EditUserPasswordOptions options);
@@ -54,71 +49,6 @@ public interface IUserController
     public Task ConfirmationRecoveryPassword(
         [Query] string email,
         [Query] string code);
-
-    [Get("/User/Properties")]
-    public Task<UserPropertyResponce> GetProperties(
-        [Body]UserPropertyOptions options);
-
-    [Get("/User/Properties/{userId}")]
-    public Task<UserPropertyResponce> GetPropeties(
-        Guid userId,
-        [Body]UserPropertyOptions options);
-
-    [Get("/User/Properties/Stacks")]
-    public Task<UserStackPropertyResponce> GetAllUserStacksProperties();
-    
-    [Get("/User/Properties/Stacks{userId}")]
-    public Task<UserStackPropertyResponce> GetAllUserStacksProperties(
-        Guid userId);
-
-    [Post("/User/Properties")]
-    public Task<UserStackPropertyResponce> SetProperties(
-        [Body] SetUserPropertyOptions options);
-
-    [Get("/User/Properties/Stacks/Names")]
-    public Task<List<string>> GetUserNamesStacks();
-    
-    [Get("/User/Properties/Stacks/Names/{userId}")]
-    public Task<List<string>> GetUserNamesStacks(
-        Guid userId);
-
-    [Get("/User/Properties/Stacks/Stack")]
-    public Task<UserStackPropertyResponce> GetUserStackProperties(
-        [Query] string stackName);
-    
-    [Get("/User/Properties/Stacks/Stack/{userId}")]
-    public Task<UserStackPropertyResponce> GetUserStackProperties(
-        Guid userId,
-        [Query] string stackName);
-
-    [Delete("/User/Properties")]
-    public Task<UserStackPropertyResponce> RemoveProperties(
-        [Body] UserPropertyOptions options);
-    
-    [Delete("/User/Properties/Stack")]
-    public Task RemoveStackProperties(
-        [Query] string stackName);
-
-    [Get("/User/Images/Avatar")]
-    public Task GetAvatar(
-        [Query] string sizeFormat);
-    
-    [Get("/User/Images/Avatar/{avatarId}")]
-    public Task GetAvatar(
-        Guid avatarId,
-        [Query] string sizeFormat);
-
-    [Post("/User/Images/Avatar")]
-    public Task<Guid> SetAvatar(
-        [Body] SetAvatarOptions options);
-
-    [Multipart]
-    [Post("/User/Images/Avatar/Form")]
-    public Task<Guid> SetAvatarFromForm(
-        [AliasAs("avatar")] StreamPart stream);
-
-    [Delete("/User/Images/Avatar")]
-    public Task<bool> DeleteAvatar();
 
     [Delete("/User")]
     public Task Delete();
