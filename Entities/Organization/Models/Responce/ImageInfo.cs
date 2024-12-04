1c557e7391a21382b29a51c28f02641cb8a9343d
Shenin Ilya <ilyashenin@mail.ru>
2024-12-04 05:15:10 +0000

    Reload

--- Entities/Organization/Models/Responce/ImageInfo.cs
+++ Entities/Organization/Models/Responce/ImageInfo.cs
@@ -5,6 +5,7 @@ namespace FG.Server.Organization.Api.Client.Models.Responce
     public class ImageInfo
     {
         public Guid Id { get; set; }
-        public String Image { get; set; } =  "" ;
+        public string Name { get; set; } = "";
+        public String Image { get; set; } =  "";
     }
 }
